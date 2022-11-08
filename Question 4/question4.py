74a243aeee67873fca08af7e1c293e74d2b8b06a
Arhan <arhanh1234@gmail.com>
2022-11-08 09:00:15 +0400

    question 4

--- Question 4/question4.py
+++ Question 4/question4.py
@@ -1,1 +1,22 @@
+# Key: 
+# SV = Speed Value1
+# DV = Distance Value
+# TV = Time Value
 
+userchoice = input("What do you want to calculate?")
+
+
+if userchoice == "speed":
+    dv = int(input("What is the distance?"))
+    tv = int(input("What is the time?"))
+    print(dv/tv)
+
+elif userchoice == "distance":
+    sv = int(input("What is the speed?"))
+    tv = int(input("What is the time?"))
+    print(sv*tv)
+
+elif userchoice == "time":
+    dv = int(input("What is the distance?"))
+    sv = int(input("What is the speed?"))
+    print(dv/sv)
